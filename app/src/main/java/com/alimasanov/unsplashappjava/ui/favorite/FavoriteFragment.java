86614b5fe6f2f4290021fa1634499116a44b5845
Alim <asanov.alim.i215@gmail.com>
2020-01-29 02:54:47 +0300

    Добавлена загрузка фото из базы данных

--- app/src/main/java/com/alimasanov/unsplashappjava/ui/favorite/FavoriteFragment.java
+++ app/src/main/java/com/alimasanov/unsplashappjava/ui/favorite/FavoriteFragment.java
@@ -11,18 +11,32 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.alimasanov.unsplashappjava.R;
+import com.alimasanov.unsplashappjava.adapters.DatabaseAdapter;
+
+import java.util.Objects;
 
 public class FavoriteFragment extends Fragment {
 
     private FavoriteViewModel dashboardViewModel;
+    private RecyclerView rv;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(FavoriteViewModel.class);
         View root = inflater.inflate(R.layout.fragment_favorite, container, false);
+        rv = root.findViewById(R.id.fav_rv);
+        rv.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        dashboardViewModel.getListPhoto().observe(Objects.requireNonNull(getActivity()), photos -> {
+            DatabaseAdapter databaseAdapter = new DatabaseAdapter(photos);
+            rv.setAdapter(databaseAdapter);
+        });
+
         return root;
     }
 }
--- app/src/main/java/com/alimasanov/unsplashappjava/ui/favorite/FavoriteViewModel.java
+++ app/src/main/java/com/alimasanov/unsplashappjava/ui/favorite/FavoriteViewModel.java
@@ -1,9 +1,39 @@
 package com.alimasanov.unsplashappjava.ui.favorite;
 
+import android.annotation.SuppressLint;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.alimasanov.unsplashappjava.App;
+import com.alimasanov.unsplashappjava.model.pojo.dbEntity.PhotoRoom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+
 public class FavoriteViewModel extends ViewModel {
 
+    private List<PhotoRoom> arrayList = new ArrayList<>();
+    private MutableLiveData<List<PhotoRoom>> listPhoto = new MutableLiveData<>();
+
+    public MutableLiveData<List<PhotoRoom>> getListPhoto() {
+        initData();
+        return listPhoto;
+    }
+
+    @SuppressLint("CheckResult")
+    private void initData() {
+        App.getInstance().getDb().getUnsplashDAO().getPhotoList()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<List<PhotoRoom>>() {
+                    @Override
+                    public void accept(List<PhotoRoom> list) throws Exception {
+                        listPhoto.setValue(list);
+                    }
+                });
+    }
 }
