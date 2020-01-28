1c9d75a6afbbdca8472a2223e2a3cc09e2f01ed7
Alim <asanov.alim.i215@gmail.com>
2020-01-28 16:01:08 +0300

    добавлены классы для работы с бд

---
+++ app/src/main/java/com/alimasanov/unsplashappjava/database/UnsplashDAO.java
@@ -0,0 +1,31 @@
+package com.alimasanov.unsplashappjava.database;
+
+import androidx.annotation.NonNull;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.alimasanov.unsplashappjava.model.pojo.Photo;
+import com.alimasanov.unsplashappjava.model.pojo.dbEntity.PhotoRoom;
+
+import java.util.List;
+
+import io.reactivex.Flowable;
+
+@Dao
+public interface UnsplashDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAll(PhotoRoom room);
+
+    @Delete
+    void delete(PhotoRoom room);
+
+    @Query("SELECT * FROM UnsplashPhoto")
+    Flowable<List<PhotoRoom>> getPhotoList();
+//
+//    @Query("SELECT photoID FROM UnsplashPhoto WHERE photoID = :photoID")
+//    @NonNull
+//    List<PhotoRoom> getPhoto(String photoID);
+}
---
+++ app/src/main/java/com/alimasanov/unsplashappjava/database/UnsplashDatabase.java
@@ -0,0 +1,11 @@
+package com.alimasanov.unsplashappjava.database;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.alimasanov.unsplashappjava.model.pojo.dbEntity.PhotoRoom;
+
+@Database(entities = {PhotoRoom.class}, version = 1, exportSchema = false)
+public abstract class UnsplashDatabase extends RoomDatabase {
+    public abstract UnsplashDAO unsplashDAO();
+}
