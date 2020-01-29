9387890470369a7f74b473a1666f48b982da5dea
Alim <asanov.alim.i215@gmail.com>
2020-01-29 20:15:12 +0300

    Добавлен запрос для получения фото по id

--- app/src/main/java/com/alimasanov/unsplashappjava/server/NetworkEndpoints.java
+++ app/src/main/java/com/alimasanov/unsplashappjava/server/NetworkEndpoints.java
@@ -6,10 +6,14 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface NetworkEndpoints {
 
     @GET("photos/random")
     Call<List<Photo>> getRandomPhotos(@Query("count") Integer count);
+
+    @GET("photos/{id}")
+    Call<Photo> getPhotoById(@Path("id") String id);
 }
