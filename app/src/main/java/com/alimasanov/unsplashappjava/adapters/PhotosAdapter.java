c4e1bcc693f6ffb67d2a2b82502455c0febe97a8
Alim <asanov.alim.i215@gmail.com>
2020-01-30 02:48:35 +0300

    Проверка на наличие фото в базе

--- app/src/main/java/com/alimasanov/unsplashappjava/adapters/PhotosAdapter.java
+++ app/src/main/java/com/alimasanov/unsplashappjava/adapters/PhotosAdapter.java
@@ -1,10 +1,12 @@
 package com.alimasanov.unsplashappjava.adapters;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TabHost;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -20,6 +22,12 @@ import com.alimasanov.unsplashappjava.model.pojo.dbEntity.PhotoRoom;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
+
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
 
 public class PhotosAdapter extends RecyclerView.Adapter<PhotosAdapter.ViewHolder> {
 
@@ -38,6 +46,7 @@ public class PhotosAdapter extends RecyclerView.Adapter<PhotosAdapter.ViewHolder
         return new ViewHolder(view);
     }
 
+    @SuppressLint("CheckResult")
     @Override
     public void onBindViewHolder(@NonNull PhotosAdapter.ViewHolder holder, int position) {
         final Photo photo = photoList.get(position);
@@ -46,13 +55,26 @@ public class PhotosAdapter extends RecyclerView.Adapter<PhotosAdapter.ViewHolder
                 .into(holder.imageView);
 
         //Добавление фото в избранное
+        //при наличии фото в базе выполнится метод onSuccess
+        //иначе выполнится метод onFailure
         holder.cardView.setOnLongClickListener(v -> {
-
-
             UnsplashDatabase db = App.getInstance().getDb();
             PhotoRoom room = new PhotoRoom(photo);
-            db.getUnsplashDAO().insertAll(room);
-            Toast.makeText(App.getInstance(), R.string.photo_added, Toast.LENGTH_SHORT).show();
+            db.getUnsplashDAO().getPhotoById(room.getPhotoID())
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new DisposableSingleObserver<PhotoRoom>() {
+                        @Override
+                        public void onSuccess(PhotoRoom photoRoom) {
+                            Toast.makeText(App.getInstance(), R.string.photo_contains, Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onError(Throwable e) {
+                            db.getUnsplashDAO().insertAll(room);
+                            Toast.makeText(App.getInstance(), R.string.photo_added, Toast.LENGTH_SHORT).show();
+                        }
+                    });
             return true;
         });
 
