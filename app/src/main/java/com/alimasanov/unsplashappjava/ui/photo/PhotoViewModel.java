d6108445b8d29a97c69821fd53a736c65ef8d5ae
Alim <asanov.alim.i215@gmail.com>
2020-01-30 15:01:42 +0300

    изменил количество итераций для загрузки 50 фото

--- app/src/main/java/com/alimasanov/unsplashappjava/ui/photo/PhotoViewModel.java
+++ app/src/main/java/com/alimasanov/unsplashappjava/ui/photo/PhotoViewModel.java
@@ -32,7 +32,7 @@ public class PhotoViewModel extends ViewModel {
     }
 
     public MutableLiveData<List<Photo>> getMutableLiveData() {
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 5; i++) {
             initData();
         }
         return mutableLiveData;
